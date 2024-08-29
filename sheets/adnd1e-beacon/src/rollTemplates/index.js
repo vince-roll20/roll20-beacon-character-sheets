f32a28bbff5b1b4eba96080010351502af449fa2
∇ince <vince-roll20@users.noreply.github.com>
2024-08-29 15:52:22 -0700

    lint

--- sheets/adnd1e-beacon/src/rollTemplates/index.js
+++ sheets/adnd1e-beacon/src/rollTemplates/index.js
@@ -1,5 +1,5 @@
-import handlebars from 'handlebars'
-import template from './index.hbs?raw'
+import handlebars from 'handlebars';
+import template from './index.hbs?raw';
 
 /*
  Currently this quickstart features a single type of roll template, that can take any parameters.
@@ -12,8 +12,8 @@ import template from './index.hbs?raw'
 
 const rollTemplates = {
   base: handlebars.compile(template)
-}
+};
 
 export const createRollTemplate = (parameters) => {
-  return rollTemplates.base(parameters)
-}
+  return rollTemplates.base(parameters);
+};
