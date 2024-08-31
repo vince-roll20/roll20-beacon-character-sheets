751c7c6bb5d1f934bd5222180cee506d9310537f
∇ince <vince-roll20@users.noreply.github.com>
2024-08-31 12:59:51 -0700

    added isLess expression

--- sheets/adnd1e-beacon/src/rollTemplates/expressions/index.js
+++ sheets/adnd1e-beacon/src/rollTemplates/expressions/index.js
@@ -3,5 +3,6 @@ export {getDice} from './getDice.js';
 export {isArray} from './isArray.js';
 export {isEqual} from './isEqual.js';
 export {isGreater} from './isGreater.js';
+export {isLess} from './isLess.js';
 export {makeBEMModifiers} from './modifiers.js';
 export {sumComponents} from './sumComponents.js';
---
+++ sheets/adnd1e-beacon/src/rollTemplates/expressions/isLess.js
@@ -0,0 +1,1 @@
+export const isLess = (arg1, arg2) => arg1 < arg2;
